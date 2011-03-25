096c799660699a111cc38cc3af9b8565ede1c3c2
Arek Flinik <blax@MacBook-Pro-Arek-Flinik.local>
2011-03-25 23:03:03 +0100

    merged game engine with client side websocket

--- public/client.js
+++ public/client.js
@@ -1,8 +1,11 @@
 var socket = new io.Socket("localhost"); 
 socket.connect();
 socket.on('connect', function () {
-  socket.send("dupa")
-}) 
+  socket.send("dupa");
+}); 
 socket.on('message', function (msg) {
-  alert(msg)
-}) 
+  alert(msg);
+}); 
+var node_send = function(msg){
+	socket.send(msg);
+};
