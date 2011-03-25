9abe16120ae39c91a198cd563d19fcd1aed18e72
Jan Dudek <jd@jandudek.com>
2011-03-25 23:05:02 +0100

    Some server stuff

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 var socket = new io.Socket("localhost"); 
 socket.connect();
 socket.on('connect', function () {
-  socket.send("dupa")
+  socket.send({ cursorPosition: { x: 100, y: 120 }})
 }) 
 socket.on('message', function (msg) {
   alert(msg)
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// require.paths.unshift('.');
 var http = require('http'),
     io = require('socket.io'),
     static = require('node-static');
+
+// var app = new application.App()
 
 var fileServer = new static.Server('./public');
 
@@ -11,7 +14,39 @@ var server = http.createServer(function (req, res) {
 });
 server.listen(3000);
 
+var app = {
+  init: function () {
+    this.count = 0
+    this.players = []
+  },
+  addPlayer: function (socketClient) {
+    var i = this.count++
+    var newPlayer = { socketClient: socketClient, cursorPosition: { x: 0, y: 0 }}
+    this.players[i] = newPlayer
+    newPlayer.updatePosition = function (newPosition) {
+      this.cursorPosition.x = newPosition.x
+      this.cursorPosition.y = newPosition.y
+      console.log("player: " + i + " ", this.cursorPosition)
+    }
+    newPlayer.destroy = function () {
+      app.players.splice(i, 1)
+      console.log("player: " + i + " destroyed")
+    }
+    return newPlayer
+  }
+};
+
+app.init();
+
 var socket = io.listen(server);
-socket.on('connection', function (client) { 
-  client.on('message', function (msg) { console.log(msg) }) 
+socket.on('connection', function (socketClient) {
+  var player = app.addPlayer(socketClient)
+  socketClient.on('message', function (msg) {
+    if (typeof msg.cursorPosition != "undefined") {
+      player.updatePosition(msg.cursorPosition)
+    }
+  })
+  socketClient.on("disconnect", function () {
+    player.destroy()
+  })
 });
